0701257a2f896a41f1121741321028d15d736410
Cristopher Encarnacion <christopher.encarnacion@gmail.com>
2022-08-02 23:07:43 -0400

    disabled cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const server = express()
-server.use(cors())
+//server.use(cors())
 
 server.get('/', (req, res) => {
     res.status(200)
